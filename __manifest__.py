f5782e7b220db2945fb63878addcdafd9f51b184
maiptn <mai.pham@on.net.vn>
2021-07-14 10:26:34 +0700

    code refactoring

--- __manifest__.py
+++ __manifest__.py
@@ -2,17 +2,6 @@
 {
     'name': "custom_calendar",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
@@ -20,19 +9,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'calendar'],
+    'depends': ['calendar'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/calendar_views.xml',
-        # 'views/templates.xml',
     ],
-    'qweb': [
-        # "static/src/xml/base.xml"
-    ],
+    'qweb': [],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'demo': [],
 }
